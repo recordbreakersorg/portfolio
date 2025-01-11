717ef960804454198fd20853123ac8700cd726ad
ken-morel <engonken8@gmail.com>
2025-01-11 14:54:25 +0100

    Reinit yarn&sass

--- eslint.config.js
+++ eslint.config.js
@@ -4,12 +4,13 @@ import { includeIgnoreFile } from '@eslint/compat';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
+import ts from 'typescript-eslint';
 const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
+export default ts.config(
   includeIgnoreFile(gitignorePath),
   js.configs.recommended,
+  ...ts.configs.recommended,
   ...svelte.configs["flat/recommended"],
   prettier,
   ...svelte.configs['flat/prettier'],
@@ -20,5 +21,14 @@ export default [
 	    ...globals.node
 	  }
 	}
+  },
+  {
+    files: ["**/*.svelte"],
+
+    languageOptions: {
+	  parserOptions: {
+	    parser: ts.parser
+	  }
+	}
   }
-];
+);
--- src/routes/demo/paraglide/+page.svelte
+++ src/routes/demo/paraglide/+page.svelte
@@ -1,13 +1,11 @@
-<script>
+<script lang="ts">
+	import type { AvailableLanguageTag } from '$lib/paraglide/runtime';
 	import { i18n } from '$lib/i18n';
 	import { page } from '$app/state';
 	import { goto } from '$app/navigation';
 	import * as m from '$lib/paraglide/messages.js';
 	
-	/** 
-	* @param {import("$lib/paraglide/runtime").AvailableLanguageTag} newLanguage 
-	*/ 
-	function switchToLanguage(newLanguage) {
+	function switchToLanguage(newLanguage: AvailableLanguageTag) {
 		const canonicalPath = i18n.route(page.url.pathname);
 		const localisedPath = i18n.resolveRoute(canonicalPath, newLanguage);
 		goto(localisedPath);
