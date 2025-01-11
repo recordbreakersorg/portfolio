ba94cdcfc193301374e4956beb3a633a5ef9f5bb
ken-morel <engonken8@gmail.com>
2025-01-11 14:35:10 +0100

    Initialized with yarn and added sass

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# portfolio
-Trailsetters portfolio website
+# The Record Breakers portfolio
---
+++ src/routes/demo/paraglide/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { i18n } from '$lib/i18n';
+	import { page } from '$app/state';
+	import { goto } from '$app/navigation';
+	import * as m from '$lib/paraglide/messages.js';
+	
+	/** 
+	* @param {import("$lib/paraglide/runtime").AvailableLanguageTag} newLanguage 
+	*/ 
+	function switchToLanguage(newLanguage) {
+		const canonicalPath = i18n.route(page.url.pathname);
+		const localisedPath = i18n.resolveRoute(canonicalPath, newLanguage);
+		goto(localisedPath);
+	}
+</script>
+
+
+
+<h1>{m.hello_world({ name: 'SvelteKit User' })}</h1>
+<div>
+	<button onclick={() => switchToLanguage('en-cm')}>en-cm</button>
+	<button onclick={() => switchToLanguage('fr-cm')}>fr-cm</button>
+</div>
